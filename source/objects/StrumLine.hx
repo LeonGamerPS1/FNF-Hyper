cd18ff104fd8734e9a4fae898969684b9895f9b5
sshmaster666 <fnfmaker666@gmail.com>
2024-11-06 17:48:33 +0100

    Things Added, Changed: - song coverter, song format - note hit detection stuff and more - new fuckin vocals too

--- source/objects/StrumLine.hx
+++ source/objects/StrumLine.hx
@@ -5,37 +5,45 @@ import flixel.FlxObject;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import haxe.ds.StringMap;
 
-class StrumLine extends FlxTypedGroup<StrumNote> {
+class StrumLine extends FlxTypedGroup<StrumNote>
+{
 	public var x:Float = 0;
 	public var y:Float = 0;
 
+	public var playableStrumLine:Bool = false;
+
 	public var lanes(default, set):Int = 0;
 
-	public function new(lanes:Int = 4, X:Float = 0, Y:Float = 0) {
+	public function new(lanes:Int = 4, X:Float = 0, Y:Float = 0)
+	{
 		super();
 		setPos(X, Y);
 		this.lanes = lanes;
 		genArrows(lanes);
 	}
 
-	function setPos(X:Float, Y:Float) {
+	function setPos(X:Float, Y:Float)
+	{
 		x = X;
 		y = Y;
 	}
 
-	function set_lanes(value:Int):Int {
+	function set_lanes(value:Int):Int
+	{
 		if (value == lanes)
 			return lanes;
 
 		return value;
 	}
 
-	function genArrows(value:Int) {
+	function genArrows(value:Int)
+	{
 		killMembers();
 		destroyMembers();
-		for (i in 0...value) {
+		for (i in 0...value)
+		{
 			var strumNote:StrumNote = new StrumNote(60 * value, i);
-			strumNote.animation.play('static');
+			strumNote.playAnim('static');
 			add(strumNote);
 		}
 	}
@@ -44,22 +52,33 @@ class StrumLine extends FlxTypedGroup<StrumNote> {
 	 * Calls `destroy()` on the group's  `members`.
 	 * @since 5.6.1
 	 */
-	public function destroyMembers():Void {
-		for (basic in members) {
+	public function destroyMembers():Void
+	{
+		for (basic in members)
+		{
 			if (basic != null)
 				basic.destroy();
 		}
 	}
 
-	override function update(elapsed:Float) {
+	override function update(elapsed:Float)
+	{
 		super.update(elapsed);
 		var i = 0;
-		forEach(function(strumNote:StrumNote) {
+		forEach(function(strumNote:StrumNote)
+		{
 			strumNote.x = x + (strumNote.width * 0.7 * i);
 			strumNote.y = y;
 			strumNote.camera = camera;
 			strumNote.cameras = cameras;
+			// ! i am too lazy to like uuuuuuuuuuuuuuuuuuuuhm add daline thing to strumnotes aswell boyyyyyyyyyyyyyyyyyyyyy
 			i++;
+
+			if (!playableStrumLine
+				&& strumNote.animation.curAnim != null
+				&& strumNote.animation.curAnim.finished
+				&& strumNote.animation.curAnim.name == 'confirm')
+				strumNote.playAnim('static', true);
 		});
 	}
 }
--- source/song/Song.hx
+++ source/song/Song.hx
@@ -3,56 +3,49 @@ package song;
 import haxe.Json;
 import openfl.utils.Assets;
 
-typedef SwagSong = {
+using StringTools;
+
+typedef SwagSong =
+{
 	var title:String;
-	var notes:Array<{
-		strumTime:Float,
-		noteData:Int,
-		mustPress:Bool,
-		sustainLength:Float
-	}>;
+	var sections:Array<SwagSection>;
+
+	var needsVoices:Bool;
+	
 	var meta:{Artist:String, Speed:Float, BPM:Float};
 }
 
-class Song {
-	public static function parseSong(path:String):SwagSong {
-		var jsonData = Json.parse(Assets.getText(path));
-
-		var song:SwagSong = {
-			title: jsonData.title,
-			notes: [],
-			meta: {
-				Artist: jsonData.meta.Artist,
-				Speed: Math.min(10, Math.max(0, jsonData.meta.Speed)), // Ensure Speed is between 0 and 10,
-				BPM: jsonData.meta.BPM
-			}
-		};
-
-		// Parse each note in the notes array
-		var json = jsonData;
-		var notes:Array<{
+typedef SwagSection =
+{
+	var notes:Array<
+		{
 			strumTime:Float,
-			noteData:Int,
-			mustPress:Bool,
+			lane:Int,
+			daLine:Int,
 			sustainLength:Float
-		}> = json.notes;
+		}>;
+}
 
-		for (note in notes) {
-			song.notes.push({
-				strumTime: note.strumTime,
-				noteData: note.noteData,
-				mustPress: note.mustPress,
-				sustainLength: note.sustainLength
-			});
+class Song
+{
+	public static function parseSong(path:String):SwagSong
+	{
+		var rawJson = Assets.getText(path);
+		while (!rawJson.endsWith("}"))
+		{
+			rawJson = rawJson.substr(0, rawJson.length - 1);
+			// LOL GOING THROUGH THE BULLSHIT TO CLEAN IDK WHATS STRANGE
 		}
+
+		var jsonData:SwagSong = Json.parse(rawJson);
 
 		#if sys
 		if (Sys.args().contains('--v')
 			|| Sys.args().contains('--verbose')
 			|| Sys.args().contains('-v')
 			|| Sys.args().contains('-verbose'))
-			trace(song);
+			trace(jsonData);
 		#end
-		return song;
+		return cast jsonData;
 	}
 }
